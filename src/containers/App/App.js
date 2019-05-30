5b084575d6023ebf33d1629cff335b915059bee6
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-30 11:41:11 -0600

    Create fetchanything file and method, update fetch withi App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-
+import fetchAnything from '../../utils/apiFetches/fetchAnything';
 export class App extends Component {
   async componentDidMount() {
-    const testFetch = await fetch('https://cors-anywhere.herokuapp.com/https://spacelaunchnow.me/api/3.3.1/astronaut/617/')
-    console.log(testFetch.json());
+    // const url = 'https://spacelaunchnow.me/api/3.3.1/launch/upcoming';
+    // const upcomingLaunches = await fetchAnything(url)
+    // console.log(upcomingLaunches);
   }
   render() {
     return (
