62cb3d90c29c85ccf37f50fa864152a151ae9383
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-30 14:19:55 -0600

    Setup NextLaunch component

---
+++ src/containers/NextLaunch/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class NextLaunch extends Component {
+  render() {
+    return (
+      <section>
+        <h3>ROCKET LAUNCH</h3>
+      </section>
+    )
+  }
+}
+
+
+
+
+export default NextLaunch
