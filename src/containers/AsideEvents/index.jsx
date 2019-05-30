3f89f5019045bf6309d3adce1b9d11864c719ade
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-30 13:08:31 -0600

    Setup AsideEvents component

---
+++ src/containers/AsideEvents/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class AsideEvents extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <aside>
+        
+      </aside>
+    )
+  }
+}
+
+export default AsideEvents
