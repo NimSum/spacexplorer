372f1c13465875a4d79aac98af7177df99c2489d
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-30 13:05:02 -0600

    Create add space events action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const addUpcomingLaunches = launches => ({
   type: 'ADD_UPCOMING_LAUNCHES',
   launches
 })
+
+export const addSpaceEvents = launches => ({
+  type: 'ADD_SPACE_EVENTS',
+  launches
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import upcomingLaunchesReducer from './upcomingLaunchesReducer';
+import spaceRelatedEventsReducer from './spaceRelatedEventsReducer'
 
 const rootReducer = combineReducers({
-  upcomingLaunches: upcomingLaunchesReducer
+  upcomingLaunches: upcomingLaunchesReducer,
+  upcomingSpaceEvents: spaceRelatedEventsReducer
 });
 
 export default rootReducer;
