1eaa3ccdbbfda336ad7021e77ffa7225a758d437
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-30 08:48:32 -0600

    Test fetch api, use cors-anywhere to bypass cors error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  async componentDidMount() {
+    const testFetch = await fetch('https://cors-anywhere.herokuapp.com/https://spacelaunchnow.me/api/3.3.1/astronaut/617/')
+    console.log(testFetch.json());
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
