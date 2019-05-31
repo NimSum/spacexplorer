626be4e589e7e48055b64da829e5bcfd512e6b08
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-31 14:59:34 -0600

    Create  show menu state and menu generator method

--- src/containers/CategoryMenu/index.jsx
+++ src/containers/CategoryMenu/index.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export class CategoryMenu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  generateMenu() {
+    return (
+      <nav>
+
+      </nav>
+    )
+  }
+
   render() {
     return (
       <div>
-        
+        <p>Xplore Space</p>
       </div>
     )
   }
