824e6615e1406454a9b93203826ee33e39562988
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-31 17:44:13 -0600

    Convert to functional component

---
+++ src/containers/NextLaunchInfo/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NextLaunchInfo  = (props) => {
+    return (
+      <article>
+        <h3>HIIIIIIIIIII</h3>
+      </article>
+    )
+}
+
+export default NextLaunchInfo;
