254ca2709d19e7210c00f61c15a09a9830581f59
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-01 13:22:35 -0600

    Add more info to next launch card, create

--- src/containers/NextLaunchCard/index.jsx
+++ src/containers/NextLaunchCard/index.jsx
@@ -3,21 +3,27 @@ import { connect } from 'react-redux';
 import { toggleLaunchInfo, addSelectedLaunch } from '../../actions';
 
 const NextLaunchCard = ({ launch, toggleLaunchInfo, updateSelectedLaunch }) => {
-  const { name, pad, status, mission, rocket } = launch;
-  console.log(rocket.configuration.image_url)
+  const { pad, status, mission, rocket } = launch;
   const showSelectedLaunch = () => {
     toggleLaunchInfo(true);
     updateSelectedLaunch(launch);
   }
   return (
     <article className='next-launch-card'>
-      <h3>{ name }</h3>
-      <p>Orbit: { mission ? mission.orbit : 'Uknown' }</p>
-      <p>Status: { status.name }</p>
-      <p>Location: { pad.location.name }</p>
-      <button onClick={ showSelectedLaunch }>
-        More
-      </button>
+      <div className="image-side">
+        <h3>{ rocket.configuration.name }</h3>
+        <img src={ rocket.configuration.image_url } alt="rocket" />
+      </div>
+      <div className="info-side">    
+        <h3>{ launch.net }</h3>
+        <p>Mission: { mission ? mission.name : 'Unknown' }</p>  
+        <p>Orbit: { mission ? mission.orbit : 'Uknown' }</p>
+        <p>Status: { status.name }</p>
+        <p>Location: { pad.location.name }</p>
+        <button onClick={ showSelectedLaunch }>
+          More
+        </button>
+      </div>
     </article>
   )
 }
