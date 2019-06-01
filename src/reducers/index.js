ac3d401a9cfe12145890c7a70e10a99163ea6b96
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-31 19:42:26 -0600

    Add rockets, space stations, orbiters, astronauts, launch providers reducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,22 @@ import upcomingLaunchesReducer from './upcomingLaunchesReducer';
 import spaceRelatedEventsReducer from './spaceRelatedEventsReducer'
 import selectedLaunchReducer from './selectedLaunchReducer';
 import toggleLaunchInfoReducer from './toggleLaunchInfoReducer';
+import rocketsReducer from './rocketsReducer';
+import spaceStationsReducer from './spaceStationsReducer';
+import orbitersReducer from './orbitersReducer';
+import astronautsReducer from './astronautsReducer';
+import launchProvidersReducer from './launchProvidersReducer'
 
 const rootReducer = combineReducers({
   upcomingLaunches: upcomingLaunchesReducer,
   upcomingSpaceEvents: spaceRelatedEventsReducer,
   selectedLaunch: selectedLaunchReducer,
-  showLaunchInfo: toggleLaunchInfoReducer
+  showLaunchInfo: toggleLaunchInfoReducer,
+  rockets: rocketsReducer,
+  spaceStations: spaceStationsReducer,
+  orbiters: orbitersReducer,
+  astronauts: astronautsReducer,
+  launchProviders: launchProvidersReducer
 });
 
 export default rootReducer;
