b34a695c858e6be3e66bf5aced43abf0f853ddf8
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-31 19:37:39 -0600

    Create launch providers  reducer

---
+++ src/reducers/launchProviders.js
@@ -0,0 +1,6 @@
+const launchProvidersReducer = (state = [], action) => {
+  if (action.type === 'ADD_LAUNCH_PROVIDERS') return action.launchProviders;
+  return state;
+}
+
+export default launchProvidersReducer;
