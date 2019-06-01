bb34c4b6e461a903cae7dd3f3fa5c834cbf3f3a6
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-31 21:55:36 -0600

    Create method to check and update rendered cards array

--- src/containers/CategorySection/index.jsx
+++ src/containers/CategorySection/index.jsx
@@ -5,23 +5,31 @@ import categoryManager from '../../thunks/categoryManager';
 export class CategorySection extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      renderedCards: []
+    }
   }
 
+  checkAndUpdate() {
+    const categorySelectedArray = this.props[this.props.category];
+    if (categorySelectedArray.next) this.generateCards();
+  }
+
+
+
   componentDidMount() {
-    console.log(this.props[this.props.category])
+    this.checkAndUpdate();
   }
   
   componentDidUpdate(prevProps) {
-    console.log(prevProps.category !== this.props.category)
-    console.log('hi')
-    console.log(this.props[this.props.category])
+    if(prevProps.category !== this.props.category) this.checkAndUpdate();
   }
 
   render() {
     return (
       <section className='category-section'>
         { this.props.category }
+        { this.state.renderedCards }
       </section>
     )
   }
