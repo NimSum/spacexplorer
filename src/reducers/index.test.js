f2194900319dfa23402bb0b4fe154a7a392bbf80
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-02 16:47:44 -0600

    Setup testing suite for ALL reducers

---
+++ src/reducers/index.test.js
@@ -0,0 +1,15 @@
+import upcomingLaunchesReducer from './upcomingLaunchesReducer';
+import spaceRelatedEventsReducer from './spaceRelatedEventsReducer'
+import selectedLaunchReducer from './selectedLaunchReducer';
+import toggleLaunchInfoReducer from './toggleLaunchInfoReducer';
+import rocketsReducer from './rocketsReducer';
+import spaceStationsReducer from './spaceStationsReducer';
+import orbitersReducer from './orbitersReducer';
+import astronautsReducer from './astronautsReducer';
+import launchProvidersReducer from './launchProvidersReducer';
+import * as mockData from '../utils/mockData';
+import * as action from '../actions';
+
+describe('Reducers', () => {
+  
+})
