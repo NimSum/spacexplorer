7c02b5069ed226bbf61ecf9f28ebfe0fc36a7ff2
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-02 11:08:10 -0600

    Component and default state testing

--- src/components/CategoryMenu/index.test.js
+++ src/components/CategoryMenu/index.test.js
@@ -4,12 +4,19 @@ import CategoryMenu from './index';
 
 describe('CategoryMenu', () => {
   const defaultState = {
-    loading: false,
-    error: '',
-    events: []
+    showMenu: false
   }
+
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(< CategoryMenu />)
+  })
+  
+  it('should match component snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have default state', () => {
+    expect(wrapper.state()).toEqual(defaultState);
   })
 })
