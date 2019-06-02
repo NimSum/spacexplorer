fe216e06788b9be5ed272be857e6ab6dc16c50ef
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-02 12:17:55 -0600

    Setup testing suite for category section

--- src/containers/CategorySection/index.test.js
+++ src/containers/CategorySection/index.test.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { shallow } from 'enzyme';
 import categoryManager from '../../thunks/categoryManager';
 import { CategorySection, mapDispatchToProps, mapStateToProps } from './index';
+import { mockAstronauts } from '../../utils/mockData';
 
 describe('CategorySection', () => {
   const defaultState = {
@@ -8,4 +10,27 @@ describe('CategorySection', () => {
     cardInfoToRender: {},
     showInfo: false
   }
+  let mockAstronauts = mockAstronauts;
+  let mockCategory = 'astronauts';
+  let mockSelectCategory = jest.fn();
+  let mockLaunch_providers = {};
+  let mockOrbiters = {};
+  let mockRockets = {};
+  let mockSpace_stations = {};
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      < CategorySection 
+        astronauts={ mockAstronauts }
+        category={ mockCategory }
+        selectCategory={ mockSelectCategory }
+        launch_providers={ mockLaunch_providers }
+        orbiters={ mockOrbiters }
+        rockets={ mockRockets }
+        space_stations={ mockSpace_stations }
+      />
+    )
+  })
+
+  
 })
