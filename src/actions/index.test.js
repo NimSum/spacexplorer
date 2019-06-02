2129b5ccfdd12b10ece50c6cc81fad3dd98650c5
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-02 15:51:02 -0600

    Update data import as all for easier readability

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,13 +1,13 @@
 import * as action from './index';
-import { mockLaunches, mockAstronauts, mockEvents } from '../utils/mockData';
+import * as mockData from '../utils/mockData';
 
 describe('Actions', () => {
   describe('ADD_UPCOMING_LAUNCHES', () => {
     it('should return object with valid action type and payload', () => {
-      const result = action.addUpcomingLaunches(mockLaunches);
+      const result = action.addUpcomingLaunches(mockData.mockLaunches);
       const expected = {
         type: 'ADD_UPCOMING_LAUNCHES',
-        launches: mockLaunches
+        launches: mockData.mockLaunches
       };
       expect(result).toEqual(expected);
     })
@@ -15,10 +15,10 @@ describe('Actions', () => {
 
   describe('ADD_SPACE_EVENTS', () => {
     it('should return object with valid action type and payload', () => {
-      const result = action.addSpaceEvents(mockEvents);
+      const result = action.addSpaceEvents(mockData.mockEvents);
       const expected = {
         type: 'ADD_SPACE_EVENTS',
-        events: mockEvents
+        events: mockData.mockEvents
       };
       expect(result).toEqual(expected);
     })
@@ -26,7 +26,7 @@ describe('Actions', () => {
 
   describe('ADD_SELECTED_LAUNCH', () => {
     it('should return object with valid action type and payload', () => {
-      const mockEvent = mockEvents.results[2];
+      const mockEvent = mockData.mockEvents.results[2];
       const result = action.addSelectedLaunch(mockEvent)
       const expected = {
         type: 'ADD_SELECTED_LAUNCH',
@@ -36,5 +36,25 @@ describe('Actions', () => {
     })
   })
 
+  describe('TOGGLE_LAUNCH_INFO', () => {
+    it('should return object with valid action type and payload', () => {
+      const result = action.toggleLaunchInfo(true)
+      const expected = {
+        type: 'TOGGLE_LAUNCH_INFO',
+        bool: true
+      };
+      expect(result).toEqual(expected);
+    })
+  })  
+
+  // describe('ADD_ROCKETS', () => {
+  //   it('should return object with valid action type and payload', () => {
+  //     const result = action.addRockets(mockR)
+  //     const expected = {
+        
+  //     };
+  //     expect(result).toEqual(expected);
+  //   })
+  // })
 
 })
