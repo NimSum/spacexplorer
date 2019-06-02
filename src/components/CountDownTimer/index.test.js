28ef0c193890dd19730b33acf6c94dc3e38d6411
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-01 22:31:35 -0600

    Snapshot testing and componentDidmount result

--- src/components/CountDownTimer/index.test.js
+++ src/components/CountDownTimer/index.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import CountDownTimer from './index';
 
 describe('CountDownTimer', () => {
-  const mockDate = '2020-06-15T00:00:00Z';
+  const mockDate = '2019-06-15T00:00:00Z';
 
   let wrapper;
   beforeEach(() => {
@@ -13,5 +13,11 @@ describe('CountDownTimer', () => {
       />)
   })
 
- 
+  it('should match component snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should should set state calculated difference on mount', () => {
+    expect(typeof wrapper.state().totalTime).toEqual('number')
+  })
 })
