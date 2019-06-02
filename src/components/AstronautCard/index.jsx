e3edcdb86b9c6b1329cd6618a787bc6ec7cd1ed0
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-01 20:06:52 -0600

    Add styling to astronaut card

--- src/components/AstronautCard/index.jsx
+++ src/components/AstronautCard/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import defaultAstronautImg  from '../../images/default-astronaut.svg';
 
 const AstronautCard = ({ astronaut, showInfo }) => {
-  const { name } = astronaut;
+  const { name, status, nationality, profile_image_thumbnail, date_of_birth } = astronaut;
   return (
-    <article>
-      <h3>{ name }</h3>
-      <button onClick={() => showInfo(astronaut, true)}>
-        more
-      </button>
+    <article className="astronaut-card">
+      <div className="image-side">
+        <img src={ profile_image_thumbnail || defaultAstronautImg } alt=""/>
+        <button onClick={() => showInfo(astronaut, true)}>
+          BIO
+        </button>
+      </div>
+      <div className="details-side">
+        <h3>{ name }</h3>
+        <p>DOB: <span>{ date_of_birth }</span></p>
+        <p>Status: <span>{ status.name }</span></p>
+        <p>Nationality: <span>{ nationality }</span></p>
+      </div>
     </article>
   )
 }
