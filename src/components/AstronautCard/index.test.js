be00ead678ed350a99a2684530d4dc40c760d9fa
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-01 21:54:25 -0600

    Create snapshot testing for astronaut card

---
+++ src/components/AstronautCard/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AstronautCard from './index';
+import mockAstronaut from '../../utils/mockData'
+
+describe('AstronautCard', () => {
+
+  const mockShowInfo = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      < AstronautCard 
+        astronaut={ mockAstronaut }
+        showInfo={ mockShowInfo }
+      />)
+  })
+
+  it('should match component snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
+
