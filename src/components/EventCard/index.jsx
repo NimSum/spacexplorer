5d48ba2a77e49227a7d60695322a18959b0be5d3
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-03 09:00:10 -0600

    Add details box for upcoming events

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
 const EventCard = ({ event }) => {
+  const [ show, showInfo ] = useState(false);
   const { name, location, news_url } = event;
   const date = moment(event.date).format('MMM DD LT');
   return (
     <article className='event-card'>
-      <h3>{ name }</h3>
-      <p>Event Type: <span>{ event.type.name }</span> </p>
-      <p>When: <span>{ date }</span></p>
-      <p>Where: <span>{ location }</span></p>
-      <a href={ news_url }>News Link</a>
-    </article>
+      <div>
+        <h3>{ name }</h3>
+        <p>Event Type: <span>{ event.type.name }</span> </p>
+        <p>When: <span>{ date }</span></p>
+        <p>Where: <span>{ location }</span></p>
+        <a href={ news_url }>News Link</a>
+      </div>
+      {
+        show && (<div className="news-details">
+        <h3>DETAILSSSSSSSSSSSSS</h3>
+        <p>Event Type: <span>{ event.type.name }</span> </p>
+        <p>When: <span>{ date }</span></p>
+        <p>Where: <span>{ location }</span></p>
+        <a href={ news_url }>News Link</a>
+      </div>)
+      }
+      <button onClick={() => showInfo(!show)}>{show ? 'SHOWN' : 'NOT SHOWN'}</button>
+    </article> 
   )
 }
 
