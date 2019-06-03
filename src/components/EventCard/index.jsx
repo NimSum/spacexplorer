5706fd3ff38396bbb2cd394bfa7e50a8addff114
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-03 14:43:34 -0600

    Create opacity animation mixin

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -3,28 +3,34 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 
 const EventCard = ({ event }) => {
+
   const [ show, showInfo ] = useState(false);
   const { name, location, news_url } = event;
   const date = moment(event.date).format('MMM DD LT');
+  
   return (
-    <article className='event-card'>
-      <div>
+    <article className='event-card'
+      onMouseEnter={() => showInfo(true)}
+      onMouseLeave={() => showInfo(false)}>
+      <div 
+        className='news-details'>
         <h3>{ name }</h3>
         <p>Event Type: <span>{ event.type.name }</span> </p>
         <p>When: <span>{ date }</span></p>
         <p>Where: <span>{ location }</span></p>
-        <a href={ news_url }>News Link</a>
       </div>
       {
-        show && (<div className="news-details">
-        <h3>DETAILSSSSSSSSSSSSS</h3>
-        <p>Event Type: <span>{ event.type.name }</span> </p>
-        <p>When: <span>{ date }</span></p>
-        <p>Where: <span>{ location }</span></p>
-        <a href={ news_url }>News Link</a>
+        show && (
+        <div className='news-detailed'
+          onMouseEnter={() => showInfo(true)}
+          onMouseLeave={() => showInfo(false)}>
+          <h3>DETAILSSSSSSSSSSSSS</h3>
+          <p>Event Type: <span>{ event.type.name }</span> </p>
+          <p>When: <span>{ date }</span></p>
+          <p>Where: <span>{ location }</span></p>
+          <a href={ news_url }>News Link</a>
       </div>)
       }
-      <button onClick={() => showInfo(!show)}>{show ? 'SHOWN' : 'NOT SHOWN'}</button>
     </article> 
   )
 }
