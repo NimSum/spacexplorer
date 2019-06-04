1eb4c475af65613fdb1bb93b18a8c98d09dd7865
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-04 17:30:53 -0600

    Transfer astronaut info to astronaut card for dynamic rendering

--- src/components/AstronautCard/index.jsx
+++ src/components/AstronautCard/index.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import defaultAstronautImg  from '../../images/default-astronaut.svg';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-const AstronautCard = ({ astronaut, showInfo }) => {
-  const { name, status, nationality, profile_image_thumbnail, date_of_birth } = astronaut;
+const AstronautCard = ({ astronaut, showInfo, detailed }) => {
+  const readableDate = (date) => moment(date).format('MMM DD LT');
+
+  const { name, bio, flights, profile_image, wiki, status, nationality, profile_image_thumbnail, date_of_birth } = astronaut;
+
+  const detailedInfo = () => {
+    return (
+      <section className="astronaut-info">
+        <div className="image-side">
+          <img src={ profile_image || defaultAstronautImg } alt="astronaut"/>
+          <h2>{ name }</h2>
+        </div>
+        <div className='details-side'>
+          <p className='bio'>{ bio }</p>
+          <article>
+            <h3>Missions:</h3>
+            { flights
+              ? flights.map(flight => (
+                  <div>
+                    <h3>{ flight.name }</h3>
+                    <p>For: { flight.mission.type || 'Unknown' }</p>
+                    <p>Orbit: { flight.orbit }</p>
+                    <p>When: { readableDate(flight.net) }</p>
+                    <p>Where: { flight.pad.location.name }</p>
+                  </div>))
+              : <h2>No Missions</h2>
+            }
+            <a href={ wiki }>Wiki Link</a>
+          </article>
+        </div>
+      </section>)
+  }
+
   return (
     <article className="astronaut-card">
       <div className="image-side">
@@ -24,7 +56,8 @@ const AstronautCard = ({ astronaut, showInfo }) => {
 
 AstronautCard.propTypes = {
   astronaut: PropTypes.object,
-  showInfo: PropTypes.func
+  showInfo: PropTypes.func,
+  detailed: PropTypes.bool
 }
 
 export default AstronautCard;
