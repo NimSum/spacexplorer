8af37e617f7de23f8f8bd19d843c4ebad12bd214
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-04 15:01:15 -0600

    Update 404 page content

--- src/components/Page404/index.jsx
+++ src/components/Page404/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import explodingRocket from '../../images/exploding-rocket.gif';
+import { Link } from 'react-router-dom';
 
-const Page404 = () =>  (
-  <div>
-    <h1>Nothing here</h1>
-    <h2>:((((((((</h2>
-  </div>
-)
+const Page404 = () =>  {
+
+
+  return (
+    <section className='page-404'>
+      <h1>404 Error</h1>
+      <h2>Page Not Found</h2>
+
+      <div>
+        { isShown ? <h2>Nothing here :(</h2> : undefined }
+        <img src={ explodingRocket } alt='exploding rocket'/>
+      </div>
+      <Link to='/'>
+        <span>HOME</span>
+      </Link>
+    </section>
+)}
 
 export default Page404;
