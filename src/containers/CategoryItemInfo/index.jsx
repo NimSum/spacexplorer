282d4fb597ec9d2bf0cdc625488caf1a5e7aa6cd
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-04 17:30:28 -0600

    Re factor switch statement within category item info container

--- src/containers/CategoryItemInfo/index.jsx
+++ src/containers/CategoryItemInfo/index.jsx
@@ -1,41 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import defaultAstronautImg from '../../images/default-astronaut.svg';
 import defaultRocketImage from '../../images/default-rocket-img.svg';
 import PropTypes from 'prop-types';
-import moment from 'moment';
+import AstronautCard from '../../components/AstronautCard';
 
 export const CategoryItemInfo = ({ item, category, isLoading, hasErrored }) => {
-  const readableDate = (date) => moment(date).format('MMM DD LT');
-
-  const astronautInfo = () => {
-    const { name, bio, flights, landings, profile_image, wiki, status, agency } = item;
-    return (
-      <section className="astronaut-info">
-        <div className="image-side">
-          <img src={ profile_image || defaultAstronautImg } alt="astronaut"/>
-          <h2>{ isLoading ? 'Loading...' : name }</h2>
-        </div>
-        
-        { !isLoading && (<div className='details-side'>
-          <p className='bio'>{ bio }</p>
-          <article>
-            <h3>Missions:</h3>
-            { flights
-              ? flights.map(flight => (
-                  <div>
-                    <h3>{ flight.name }</h3>
-                    <p>For: { flight.mission.type || 'Unknown' }</p>
-                    <p>Orbit: { flight.orbit }</p>
-                    <p>When: { readableDate(flight.net) }</p>
-                    <p>Where: { flight.pad.location.name }</p>
-                  </div>))
-              : <h2>No Missions</h2>
-            }
-          </article>
-        </div>)}
-      </section>)
-  }
+  
 
   const rocketInfo = () => {
     const { full_name, launch_service_provider, leo_capacity, 
@@ -55,28 +25,26 @@ export const CategoryItemInfo = ({ item, category, isLoading, hasErrored }) => {
 
 
 
-  let renderThis;
-  switch (category) {
-    case 'rockets':
-      renderThis = rocketInfo();
-    break;
-    case 'space_stations':
-      renderThis = (<div>lestations</div>)
-    break;
-    case 'orbiters':
-      renderThis = (<div>leobiters</div>)
-    break;
-    case 'astronauts':
-      renderThis = astronautInfo();
-    break;
-    default:
-      renderThis = (<div>launchhhhss</div>)
-    break;
+  const infoToRender = () => {
+    switch (category) {
+      case 'rockets':
+        return rocketInfo();
+      case 'space_stations':
+        return (<div>lestations</div>)
+      case 'orbiters':
+        return (<div>leobiters</div>)
+      case 'astronauts':
+        return < AstronautCard 
+          astronaut={ item } 
+          detailed={ true } />;
+      default:
+        return (<div>launchhhhss</div>)
+    }
   }
   
   return (
     <article className="category-info-container">
-      { renderThis }
+      { infoToRender() }
     </article>
   )
 }
