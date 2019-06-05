22fca5aa4d702e37085dfde9b7431106411a6d7b
Nimrod Garcia <nimm.garcia@gmail.com>
2019-06-04 22:10:28 -0600

    Updates

---
+++ precache-manifest.da326fca36334c97f9727b0a4d2166cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2ad8d033765842b734d653b8470778",
+    "url": "/spacexplorer/index.html"
+  },
+  {
+    "revision": "dcf193c522d50efc906f",
+    "url": "/spacexplorer/static/css/main.138081f9.chunk.css"
+  },
+  {
+    "revision": "6b9c06ee1581412c1aae",
+    "url": "/spacexplorer/static/js/2.9972d91b.chunk.js"
+  },
+  {
+    "revision": "dcf193c522d50efc906f",
+    "url": "/spacexplorer/static/js/main.bdce7256.chunk.js"
+  },
+  {
+    "revision": "003d18b3c73d584f956c",
+    "url": "/spacexplorer/static/js/runtime~main.22adbefc.js"
+  },
+  {
+    "revision": "368f55f0e065b5b4814329d71b2481ae",
+    "url": "/spacexplorer/static/media/default-astronaut.368f55f0.svg"
+  },
+  {
+    "revision": "91e4969e081b62579ae6ee6cede0350b",
+    "url": "/spacexplorer/static/media/default-rocket-img.91e4969e.svg"
+  },
+  {
+    "revision": "cbc754054bf387e8da3828cedd6585e3",
+    "url": "/spacexplorer/static/media/exploding-rocket.cbc75405.gif"
+  },
+  {
+    "revision": "c2897a59894d9367dc8a9a30427afaea",
+    "url": "/spacexplorer/static/media/rocket-launch.c2897a59.gif"
+  }
+]);
